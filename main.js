96fb4866a6a32dd65a5ca573e55735e59ffa861c
Owen G <garciafamilyid4@gmail.com>
2025-05-26 12:51:37 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    function loadScript(url, callback) {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = callback;
+        document.head.appendChild(script);
+    }
+
+    function loadMapLibre(callback) {
+        loadScript("https://unpkg.com/maplibre-gl@5.5.0/dist/maplibre-gl.js", function() {
+            console.log("MapLibre loaded");
+            callback();
+        });
+    }
+
+    function checkAndRunAircraftScript() {
+        const aircraftName = geofs.aircraftList[geofs.aircraft.instance.id]?.name;
+        console.log("Current Aircraft:", aircraftName);
+
+        const aircraftScripts = {
+            "Beechcraft Baron B55": "https://owennewo-dev.github.io/GeoFS-cockpit-realism/b55/b55.js",
+        };
+
+        if (aircraftScripts[aircraftName]) {
+            console.log(`Loading script: ${aircraftScripts[aircraftName]}`);
+            loadScript(aircraftScripts[aircraftName]);
+        } else {
+            console.log("Aircraft not supported");
+        }
+    }
+
+    const checkGeoFSReady = setInterval(() => {
+        if (typeof geofs !== "undefined" && geofs.aircraft && geofs.aircraft.instance) {
+            clearInterval(checkGeoFSReady);
+            loadMapLibre(checkAndRunAircraftScript);
+        }
+    }, 1000);
+})();
